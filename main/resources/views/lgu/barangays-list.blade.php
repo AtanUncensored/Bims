b48921aac2525425c68afb4f8d500b288c1c8fec
AtanUncensored <153695653+AtanUncensored@users.noreply.github.com>
2024-08-24 12:29:44 +0800

    list of barangays

--- main/resources/views/lgu/barangays-list.blade.php
+++ main/resources/views/lgu/barangays-list.blade.php
@@ -7,8 +7,22 @@
 @section('title', 'Barangays')
 
 @section('content')
-<div class="barangay-list">
-    <label>Barangay List</label>
-</div>
+<div class="barangay-list p-6">
+    <label class="text-2xl font-bold mb-4 block">Barangay List</label>
 
+    <table class="min-w-full bg-white border border-gray-300 rounded-lg">
+        <thead class="bg-gray-200">
+            <tr>
+                <th class="py-2 px-4 border-b border-gray-300 text-left">Barangay Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($barangays as $barangay)
+            <tr class="hover:bg-gray-100">
+                <td class="py-2 px-4 border-b border-gray-300">{{ $barangay->barangay_name }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
