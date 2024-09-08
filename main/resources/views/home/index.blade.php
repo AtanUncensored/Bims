38484f0ef21e4251629d7a85f3784e42519b511d
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2024-09-08 18:11:01 +0800

    Update index.blade.php

--- main/resources/views/home/index.blade.php
+++ main/resources/views/home/index.blade.php
@@ -14,10 +14,10 @@
         <img class="rounded-full" src="{{ asset('images/bims-logo.png') }}" alt="LGU logo">
     </div>
     <div class="logo-title font-bold text-start">
-        <header class="text-4xl">B<span class="text-2xl text-blue-300">ARANGAY</span></header>
-        <header class="text-4xl">I<span class="text-2xl text-blue-300">NFORMATION</span></header>
-        <header class="text-4xl">M<span class="text-2xl text-blue-300">ANAGEMENT</span></header>
-        <header class="text-4xl">S<span class="text-2xl text-blue-300">YSTEM</span></header>
+        <header class="text-5xl">B<span class="text-3xl text-blue-300">ARANGAY</span></header>
+        <header class="text-5xl">I<span class="text-3xl text-blue-300">NFORMATION</span></header>
+        <header class="text-5xl">M<span class="text-3xl text-blue-300">ANAGEMENT</span></header>
+        <header class="text-5xl">S<span class="text-3xl text-blue-300">YSTEM</span></header>
     </div>
 </div>
 <div class="login-form">
