e63498e1ad83ba99014d96886a9714b6e50c7e52
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2024-09-20 04:26:30 +0800

    responsive update

--- main/resources/views/home/index.blade.php
+++ main/resources/views/home/index.blade.php
@@ -6,24 +6,28 @@
     body {
         background-image: url('{{ asset('/images/lgu-building.jpg') }}');
         background-size: cover;
+        background-position: center;
+        background-attachment: fixed;
     }
 </style>
 
-<div class="title">
-    <div class="img-logo">
-        <img class="rounded-full" src="{{ asset('images/bims-logo.png') }}" alt="LGU logo">
+<div class="title flex flex-col items-center lg:flex-row lg:justify-center lg:items-center py-2">
+    <div class="img-logo lg:w-1/4 mb-4 lg:mb-0">
+        <img class="rounded-full w-32 h-32 lg:w-[100%] lg:h-[100%]" src="{{ asset('images/bims-logo.png') }}" alt="LGU logo">
     </div>
-    <div class="logo-title font-bold text-start">
-        <header class="text-5xl">B<span class="text-3xl text-blue-300">ARANGAY</span></header>
-        <header class="text-5xl">I<span class="text-3xl text-blue-300">NFORMATION</span></header>
-        <header class="text-5xl">M<span class="text-3xl text-blue-300">ANAGEMENT</span></header>
-        <header class="text-5xl">S<span class="text-3xl text-blue-300">YSTEM</span></header>
+    <div class="logo-title font-bold text-center lg:text-left">
+        <header class="text-4xl md:text-5xl">B<span class="text-2xl md:text-3xl text-blue-300">ARANGAY</span></header>
+        <header class="text-4xl md:text-5xl">I<span class="text-2xl md:text-3xl text-blue-300">NFORMATION</span></header>
+        <header class="text-4xl md:text-5xl">M<span class="text-2xl md:text-3xl text-blue-300">ANAGEMENT</span></header>
+        <header class="text-4xl md:text-5xl">S<span class="text-2xl md:text-3xl text-blue-300">YSTEM</span></header>
     </div>
 </div>
-<div class="login-form">
-    <label for="lgu">LGU Tubigon</label>
+
+<div class="login-form text-center lg:text-left mt-6">
+    <label for="lgu" class="text-lg lg:text-xl font-semibold">LGU Tubigon</label>
     <div>
-        <a href="/lgu-login">Log in</a>
+        <a href="/lgu-login" class="hover:text-blue-900 font-bold lg:text-xl">Log in</a>
     </div>
 </div>
+
 @endsection
--- main/resources/views/login/login-form.blade.php
+++ main/resources/views/login/login-form.blade.php
@@ -8,42 +8,31 @@
     <link rel="icon" href="{{ asset('images/bims-logo.png') }}">
     <script src="{{ asset('/js/tailwind.min.js') }}"></script>
     <style>
+        html, body {
+            height: 100%;
+            margin: 0;
+        }
         body {
+            display: flex;
+            flex-direction: column;
             background-image: url('{{ asset('/images/lgu-building.jpg') }}');
             background-size: cover;
-            margin: 0;
-            font-family: Arial, sans-serif;
-        }
-        .top-nav {
-            padding: 7px 16px;
-            font-size: 1.23rem;
-            position: fixed;
-            width: 100%;
-            color: white;
-            display: flex;
-            align-items: center;
-            justify-content: start;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-        }
-        .top-nav .logo img {
-            width: 60px;
-            height: 61px;
-            border-radius: 50%;
-        }
-        .top-nav .title a {
-            color: white;
-            list-style: none;
-            text-decoration: none;
-            margin-left: 14px;
+            background-position: center;
+            background-attachment: fixed;
         }
         .content {
             display: flex;
             align-items: center;
-            min-height: 94vh;
+            flex-grow: 1;
             padding: 60px 20px 20px;
             gap: 20px;
             justify-content: center;
             background-color: rgba(0, 0, 0, 0.5);
+            flex-direction: row;
+        }
+        .logo {
+            flex: 1;
+            max-width: 350px;
         }
         .info-login {
             flex: 2;
@@ -131,81 +120,93 @@
             animation: blink 1s infinite; 
         }
 
+        @media (max-width: 768px) { 
+            .content {
+                flex-direction: column; 
+            }
+            .logo {
+                order: 1;
+            }
+            .info-login {
+                order: 2;
+            }
+        }
     </style>
 </head>
 <body>
-    <div class="top-nav bg-blue-500">
-        <div class="logo">
+    <div class="top-nav bg-blue-500 flex justify-start py-2 px-4 items-center">
+        <div class="w-[60px] h-[60px] lg:w-[60px] lg:h-[60px]">
             <img src="{{ asset('images/bims-logo.png') }}" alt="BIMS logo">
         </div>
         <div class="title">
-            <a href="/">Barangay Information Management System</a>
+            <a href="/" class="font-bold text-white text-[15px] lg:text-xl">
+                <span class="block lg:hidden font-bold gap-2 text-3xl px-4">BIMS</span> 
+                <span class="hidden lg:block gap-2 px-4">Barangay Information Management System</span>
+            </a>
         </div>
-    </div>    
-<div class="content">
-    <div class="logo">
-        <img class="rounded-full" src="{{ asset('images/tubigon-logo.png') }}" alt="LGU logo">
+    </div>     
+    <div class="content">
+        <div class="logo">
+            <img class="rounded-full w-[200px] h-[200px] lg:w-[300px] lg:h-[300px]" src="{{ asset('images/tubigon-logo.png') }}" alt="LGU logo">
+        </div>
+        <div class="info-login">
+
+            <!-- Admin Notice -->
+            <div class="admin-notice">
+                NOTICE: This is an Admin Login
+            </div>
+            <div class="login-form">
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username" />
+                        @error('email')
+                            <span class="text-red-500 text-sm mt-2">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password:</label>
+                        <input id="password" type="password" name="password" required autocomplete="current-password" />
+                        <span class="password-toggle" onclick="togglePasswordVisibility()">
+                            <svg id="eye-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            </svg>                    
+                            <svg id="eye-slash-icon" class="hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                            </svg>                          
+                        </span>
+                        @error('password')
+                            <span class="text-red-500 text-sm mt-2">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="flex items-center justify-end mt-6">
+                        <button type="submit" class="inline-flex items-center font-bold px-4 py-2 bg-blue-600 text-white rounded">
+                            Login
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="info-login">
-
-        <!-- Admin Notice -->
-        <div class="admin-notice">
-            NOTICE: This is an Admin Login
-        </div>
-        <div class="login-form">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username" />
-                    @error('email')
-                        <span class="text-red-500 text-sm mt-2">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="password">Password:</label>
-                    <input id="password" type="password" name="password" required autocomplete="current-password" />
-                    <span class="password-toggle" onclick="togglePasswordVisibility()">
-                        <svg id="eye-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                        </svg>                    
-                        <svg id="eye-slash-icon" class="hidden" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
-                        </svg>                          
-                    </span>
-                    @error('password')
-                        <span class="text-red-500 text-sm mt-2">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="flex items-center justify-end mt-6">
-                    <button type="submit" class="inline-flex items-center font-bold px-4 py-2 bg-blue-600 text-white rounded">
-                        Login
-                    </button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-@include('templates.footer')
-
-<!-- Password hidden script ni para sa mga d kamao mo type kaayo -->
-
-<script>
-    function togglePasswordVisibility() {
-        const passwordInput = document.getElementById('password');
-        const eyeIcon = document.getElementById('eye-icon');
-        const eyeSlashIcon = document.getElementById('eye-slash-icon');
-        if (passwordInput.type === 'password') {
-            passwordInput.type = 'text';
-            eyeIcon.classList.add('hidden');
-            eyeSlashIcon.classList.remove('hidden');
-        } else {
-            passwordInput.type = 'password';
-            eyeIcon.classList.remove('hidden');
-            eyeSlashIcon.classList.add('hidden');
+    @include('templates.footer')
+    
+    <script>
+        function togglePasswordVisibility() {
+            const passwordInput = document.getElementById('password');
+            const eyeIcon = document.getElementById('eye-icon');
+            const eyeSlashIcon = document.getElementById('eye-slash-icon');
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                eyeIcon.classList.add('hidden');
+                eyeSlashIcon.classList.remove('hidden');
+            } else {
+                passwordInput.type = 'password';
+                eyeIcon.classList.remove('hidden');
+                eyeSlashIcon.classList.add('hidden');
+            }
         }
-    }
-</script>
+    </script>
 </body>
 </html>
