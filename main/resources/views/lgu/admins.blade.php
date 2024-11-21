ddca7dfe311c556dd0547d2994541a55a80fd8bc
Kyle Christian Daga-ang <153695256+KairuDagaang@users.noreply.github.com>
2024-11-21 12:18:06 +0800

    responsive lgu

--- main/resources/views/lgu/admins.blade.php
+++ main/resources/views/lgu/admins.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="barangay-admins py-2 px-4">
     <div id="title">
-        <h1 class="text-2xl font-bold text-blue-600">ADMINISTRATORS:</h1>
+        <h1 class="text-lg sm:text-2xl font-bold text-blue-600">ADMINISTRATORS:</h1>
     </div>
     <div class="my-6">
         <hr class="border-t-2 border-gray-300">
@@ -24,7 +24,7 @@
 <!-- Filter Form using Select2 -->
     <div class="mb-4"> 
       <form action="{{ route('lgu.admins') }}" method="GET" id="filterForm">
-        <p class="mb-2 text-gray-600 font-semibold">Filter by Barangay:</p>
+        <p class="mb-2 text-gray-600 text-[13px] lg:text-[15px] font-semibold">Filter by Barangay:</p>
             <select name="barangay_ids[]" id="barangay_id" class="barangay-select w-full p-2 border rounded-md" multiple>
                 @foreach($barangays as $barangay)
                     <option value="{{ $barangay->id }}" {{ in_array($barangay->id, (array) request('barangay_ids', [])) ? 'selected' : '' }}>
@@ -38,15 +38,15 @@
     <div class="mb-2 flex justify-end">
 
         <!-- Caution before proceding to delete an admin -->
-        <button class="relative text-left px-4 py-2 text-red-600 hover:text-red-800 rounded-lg group">
+        <button class="relative text-left px-4 py-2 text-red-600 text-[7px] lg:text-[15px] hover:text-red-800 rounded-lg group">
             <span class="font-bold">Caution <i class="fa-solid fa-triangle-exclamation"></i></span>
-            <div class="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 hidden group-hover:block bg-white text-red-600 text-sm p-4 rounded shadow-lg w-64">
+            <div class="absolute top-full left-1/2 transform -translate-x-1/2 mt-2 hidden group-hover:block bg-white text-red-600 text-[7px] lg:text-[15px] p-4 rounded shadow-lg w-64">
                 <strong>Warning!</strong> Beware when deleting a single admin from a specific barangay. Please Proceed with caution and read the confirmation before continuing.
             </div>
         </button>        
 
          <!-- Create -->
-        <a href="{{ route('lgu.create-barangay') }}" class="py-2 px-4 bg-blue-600 text-white font-bold rounded hover:bg-blue-500"><i class="fa-solid fa-plus"></i> Add Barangay Admin</a>
+        <a href="{{ route('lgu.create-barangay') }}" class="py-2 px-4 text-[7px] lg:text-[15px] bg-blue-600 text-white font-bold rounded hover:bg-blue-500"><i class="fa-solid fa-plus"></i> Add Barangay Admin</a>
     </div>
 
     <!-- Table for displaying Barangay Admins -->
@@ -54,10 +54,10 @@
         <table class="min-w-full table-auto bg-white border border-gray-300 rounded-lg shadow-md">
             <thead class="bg-gray-600 text-white">
                 <tr>
-                    <th class="py-3 px-6 bg-gray-600 text-white font-bold uppercase text-[10px] lg:text-sm text-left">Name</th>
-                    <th class="py-3 px-6 bg-gray-600 text-white font-bold uppercase text-[10px] lg:text-sm text-left">Email</th>
-                    <th class="py-3 px-6 bg-gray-600 text-white font-bold uppercase text-[10px] lg:text-sm text-left">Assigned Barangay</th>
-                    <th class="py-3 px-6 bg-gray-600 text-white font-bold uppercase text-[10px] lg:text-sm text-left">Edit / Delete</th>
+                    <th class="py-3 px-4 lg:px-6 bg-gray-600 text-white font-bold uppercase text-[7px] lg:text-[13px] text-left">Name</th>
+                    <th class="py-3 px-4 lg:px-6 bg-gray-600 text-white font-bold uppercase text-[7px] lg:text-[13px] text-left">Email</th>
+                    <th class="py-3 px-4 lg:px-6 bg-gray-600 text-white font-bold uppercase text-[7px] lg:text-[13px] text-left">Barangay</th>
+                    <th class="py-3 px-4 lg:px-6 bg-gray-600 text-white font-bold uppercase text-[7px] lg:text-[13px] text-left">Update</th>
                 </tr>
             </thead>
             <tbody>
@@ -70,38 +70,38 @@
                 @else
                 @foreach ($adminUsers as $admin)
                 <tr class="hover:bg-gray-300 transition duration-300 ease-in-out">
-                    <td class="py-2 px-4 font-semibold border-b border-gray-200">{{ $admin->name }}</td>
-                    <td class="py-2 px-4 font-semibold border-b text-blue-600 border-gray-200">{{ $admin->email }}</td>
-                    <td class="py-2 px-4 font-semibold border-b border-gray-200">{{ $admin->barangay ? $admin->barangay->barangay_name : 'N/A' }}</td>
+                    <td class="py-2 px-4 font-semibold border-b text-[7px] lg:text-[15px] border-gray-200">{{ $admin->name }}</td>
+                    <td class="py-2 px-4 font-semibold border-b text-[7px] lg:text-[15px] text-blue-600 border-gray-200">{{ $admin->email }}</td>
+                    <td class="py-2 px-4 font-semibold border-b text-[7px] lg:text-[15px] border-gray-200">{{ $admin->barangay ? $admin->barangay->barangay_name : 'N/A' }}</td>
                     <td class="py-2 px-4 font-semibold border-b border-gray-200">
-                        <a href="{{ route('lgu.admins-crud.edit-barangay-admin', $admin->id) }}" class="text-blue-700 py-1 px-2 md:px-3 rounded hover:text-blue-900"><i class="fa-solid fa-pen"></i></a>
+                        <a href="{{ route('lgu.admins-crud.edit-barangay-admin', $admin->id) }}" class="text-blue-600 py-1 px-2 sm:px-3 rounded hover:text-blue-800 text-sm sm:text-base"><i class="fa-solid fa-pen"></i></a>
 
-                        <button onclick="toggleDeleteModal('{{ $admin->id }}')" class="text-red-700 py-1 px-2 md:px-3 rounded hover:text-red-900">
+                        <button onclick="toggleDeleteModal('{{ $admin->id }}')" class="text-red-600 py-1 px-2 sm:px-3 rounded hover:text-red-800 text-sm sm:text-base">
                             <i class="fa-solid fa-trash"></i>
                         </button>
 
                         <!-- Delete Modal ni dere same sa log out nga layout -->
                         <div id="delete-modal-{{ $admin->id }}" class="hidden fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-20">
-                            <div class="bg-white rounded-lg shadow-lg w-full max-w-lg p-4 sm:p-6 md:w-1/2 lg:w-1/3">
+                            <div class="bg-white rounded-lg shadow-lg w-[80%] max-w-lg p-4 sm:p-6 md:w-1/2 lg:w-1/3">
                                 <div class="flex justify-start items-center mb-3">
-                                    <img class="w-[50px] h-[50px] rounded-full" src="{{ asset('images/' . $admin->barangay->logo) }}" alt="barangay/lgu logo">
-                                    <h3 class="text-lg font-bold text-green-600 ml-3 uppercase"> Brgy. {{ $admin->barangay ? $admin->barangay->barangay_name : 'N/A' }}</h3>
+                                    <img class="w-[25px] h-[25px] lg:w-[50px] lg:h-[50px] rounded-full" src="{{ asset('images/' . $admin->barangay->logo) }}" alt="barangay/lgu logo">
+                                    <h3 class="text-sm lg:text-lg font-bold text-green-600 ml-3 uppercase"> Brgy. {{ $admin->barangay ? $admin->barangay->barangay_name : 'N/A' }}</h3>
                                 </div>
-                                <h3 class="text-lg font-bold text-gray-600 mb-3">Admin user: <span class="text-blue-600">{{ $admin->name }}</span></h3>
+                                <h3 class="text-sm lg:text-lg font-bold text-gray-600 mb-3">Admin user: <span class="text-blue-600">{{ $admin->name }}</span></h3>
                                 <hr class="border-t-2 border-gray-300">
-                                <p class="mb-3 mt-3 text-red-500"><i class="fa-solid fa-triangle-exclamation"></i> Records added by this admin will also be deleted.</p>
+                                <p class="mb-3 mt-3 text-sm lg:text-lg text-red-500"><i class="fa-solid fa-triangle-exclamation"></i> Records added by this admin will also be deleted.</p>
 
-                                <p class="mb-6 ml-4 text-gray-600">Continue to delete admin?</p>
+                                <p class="mb-6 ml-4 text-sm lg:text-lg text-gray-600">Continue to delete admin?</p>
                                 
                                 <div class="flex justify-end space-x-4">
-                                    <button onclick="toggleDeleteModal('{{ $admin->id }}')" class="hover:text-gray-400">
+                                    <button onclick="toggleDeleteModal('{{ $admin->id }}')" class="hover:text-gray-400 text-[10px] lg:text-[15px]">
                                         Cancel
                                     </button>
 
                                     <form action="{{ route('lgu.admins-crud.delete-barangay-admin', $admin->id) }}" method="POST">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="py-2 px-4 text-red-800 hover:text-red-400">
+                                        <button type="submit" class="py-2 px-4 text-[10px] lg:text-[15px] text-red-800 hover:text-red-400">
                                             Delete
                                         </button>
                                     </form>
--- main/resources/views/lgu/barangays-list.blade.php
+++ main/resources/views/lgu/barangays-list.blade.php
@@ -13,48 +13,116 @@
     <hr class="border-t-2 border-gray-300 mb-4">
 
     <div class="flex items-center justify-center mt-3 mb-6"> 
-        <form class="inline-flex w-full sm:w-auto" method="GET" action="{{ route('lgu.barangays-list') }}">
-            <input type="text" name="search" placeholder="Search for a barangay" class="py-2 px-4 border border-gray-300 rounded-l-md focus:outline-none focus:ring-1 focus:ring-gray-500 w-full sm:w-64">
-            <button type="submit" class="py-2 px-4 bg-gray-600 text-white rounded-r-md hover:bg-gray-600 transition">
-                <i class="fa-solid fa-magnifying-glass"></i>
+        <form class="inline-flex w-[50%] sm:w-auto" method="GET" action="{{ route('lgu.barangays-list') }}">
+            <input 
+                type="text" 
+                name="search" 
+                placeholder="Search for a barangay" 
+                class="px-3 border border-gray-300 rounded-l-md focus:outline-none focus:ring-1 focus:ring-gray-500 w-full sm:w-64 lg:py-2 lg:px-4 text-[7px] sm:text-base">
+            <button 
+                type="submit" 
+                class="px-3 bg-gray-600 text-white rounded-r-md hover:bg-gray-600 transition lg:py-2 lg:px-4">
+                <i class="fa-solid fa-magnifying-glass fa-sm lg:fa-lg"></i>
             </button>
         </form>
     </div>
+    
 
     <!-- Table ni record sa available barangay -->
     <div class="max-h-[60vh] overflow-y-auto">
         <table class="min-w-full table-auto bg-white border border-gray-300 rounded-lg shadow-md">
             <thead class="bg-gray-600 text-white">
                 <tr>
-                    <th class="py-3 px-4 sm:px-6 bg-gray-600 text-white font-bold uppercase text-xs sm:text-sm text-left">Barangay Logo</th>
-                    <th class="py-3 px-4 sm:px-6 bg-gray-600 text-white font-bold uppercase text-xs sm:text-sm text-left">Barangay Name</th>
-                    <th class="py-3 px-4 sm:px-6 bg-gray-600 text-white font-bold uppercase text-xs sm:text-sm text-left">View / Edit</th>
+                    <th class="py-3 px-4 lg:px-6 bg-gray-600 text-white font-bold uppercase text-[7px] lg:text-[13px] text-left">Barangay Logo</th>
+                    <th class="py-3 px-4 lg:px-6 bg-gray-600 text-white font-bold uppercase text-[7px] lg:text-[13px] text-left">Barangay Name</th>
+                    <th class="py-3 px-4 lg:px-6 bg-gray-600 text-white font-bold uppercase text-[7px] lg:text-[13px] text-left">View / Edit</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($barangays as $barangay)
                 <tr class="hover:bg-gray-300 transition duration-300 ease-in-out">
-                    <td class="py-2 px-4 sm:px-6 font-semibold border-b border-gray-200">
+                    <td class="lg:py-2 lg:px-4 font-semibold border-b border-gray-200">
                         @if ($barangay->logo)
-                            <img src="{{ asset('images/' . $barangay->logo) }}" alt="Logo" class="w-8 h-8 sm:w-10 sm:h-10 object-cover rounded-full">
+                            <img src="{{ asset('images/' . $barangay->logo) }}" alt="Logo" class="w-8 h-8 sm:w-10 sm:h-10 ml-6 object-cover rounded-full">
                         @else
                             <span class="text-sm text-gray-500">No Logo</span>
                         @endif
                     </td>
 
-                    <td class="py-2 px-4 sm:px-6 font-semibold border-b border-gray-200">{{ $barangay->barangay_name }}</td>
+                    <td class="py-2 px-4 lg:py-2 lg:px-4 text-[7px] lg:text-[15px] font-semibold border-b border-gray-200">{{ $barangay->barangay_name }}</td>
                     <td class="py-2 px-4 sm:px-6 font-semibold border-b border-gray-200">
                         <a href="{{ route('lgu.barangays-show', $barangay->id) }}" class="text-gray-700 py-1 px-2 sm:px-3 rounded hover:text-gray-900 text-sm sm:text-base">
                             <i class="fa-solid fa-window-maximize"></i>
                         </a>
-                        <a href="{{ route('lgu.barangays-edit', $barangay->id) }}" class="text-blue-600 py-1 px-2 sm:px-3 rounded hover:text-blue-800 text-sm sm:text-base">
+                        {{-- <a href="{{ route('lgu.barangays-edit', $barangay->id) }}" class="text-blue-600 py-1 px-2 sm:px-3 rounded hover:text-blue-800 text-sm sm:text-base">
                             <i class="fa-solid fa-pen"></i>
-                        </a>
+                        </a> --}}
+                        <button onclick="toggleEditModal('{{ $barangay->id }}')" class="text-blue-600 py-1 px-2 sm:px-3 rounded hover:text-blue-800 text-sm sm:text-base">
+                            <i class="fa-solid fa-pen"></i>
+                        </button>
                     </td>
                 </tr>
+                <!-- Edit Modal -->
+                <div id="edit-modal-{{ $barangay->id }}" class="hidden fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-20">
+                    <div class="bg-white rounded-lg shadow-lg w-[80%] max-w-lg p-4 sm:p-6 md:w-1/2 lg:w-1/3">
+                        <div class="flex justify-between items-center mb-3">
+                            <div class="flex justify-start items-center">
+                                <img class="w-[25px] h-[25px] lg:w-[50px] lg:h-[50px] rounded-full" src="{{ asset('images/' . $barangay->logo) }}" alt="barangay logo">
+                            <h3 class="text-sm lg:text-lg font-bold text-green-600 ml-3 uppercase">Brgy. {{ $barangay->barangay_name }}</h3>
+                            </div>
+                            <div class="flex justify-end items-center">
+                                <button onclick="toggleEditModal('{{ $barangay->id }}')" class="inline-block align-baseline font-bold text-[10px] lg:text-[15px] text-gray-600 hover:text-blue-800">
+                                    <i class="fa-solid fa-xmark fa-2xl"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <h3 class="text-sm lg:text-lg font-bold text-gray-600 mb-3">Edit Barangay Details</h3>
+                        <hr class="border-t-2 border-gray-300 mb-4">
+
+                        <form action="{{ route('lgu.barangays-update', $barangay->id) }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+                
+                            <div class="mb-4">
+                                <label for="logo" class="block text-gray-700 text-sm font-bold mb-2">Logo:</label>
+                                <input type="file" id="logo" name="logo" class="mt-1 block w-full text-sm text-gray-900 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50" accept="image/*">
+                                @error('logo')
+                                    <span class="text-red-500 text-sm">{{ $message }}</span>
+                                @enderror
+                            </div>
+                
+                            <div class="mb-4">
+                                <label for="barangay_name" class="block text-gray-700 text-sm font-bold mb-2">Barangay Name:</label>
+                                <input type="text" id="barangay_name" name="barangay_name" value="{{ old('barangay_name', $barangay->barangay_name) }}" class="mt-1 block w-full text-sm text-gray-900 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50">
+                                @error('barangay_name')
+                                    <span class="text-red-500 text-sm">{{ $message }}</span>
+                                @enderror
+                            </div>
+                
+                            <div class="mb-4">
+                                <label for="background_image" class="block text-gray-700 text-sm font-bold mb-2">Background Image:</label>
+                                <input type="file" id="background_image" name="background_image" class="mt-1 block w-full text-sm text-gray-900 border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-500 focus:ring-opacity-50" accept="image/*">
+                                @error('background_image')
+                                    <span class="text-red-500 text-sm">{{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="flex justify-end items-center">
+                                    <button type="submit" class="bg-blue-600 text-[10px] lg:text-[15px] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-3">Update Barangay</button>  
+                            </div>
+                        </form>
+                    </div>
+                </div>
                 @endforeach
             </tbody>
         </table>
     </div>
 </div>
+
+<script>
+    // Pangpagawas sa modal
+    function toggleEditModal(barangayId) {
+        const modal = document.getElementById(`edit-modal-${barangayId}`);
+        modal.classList.toggle('hidden');
+    }
+</script>
 @endsection
