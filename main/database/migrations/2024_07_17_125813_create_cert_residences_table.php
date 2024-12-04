b6e94f66de9ddb320ce553ec0e0331b565e2ddc4
AtanUncensored <153695653+AtanUncensored@users.noreply.github.com>
2024-12-04 22:11:20 +0800

    add og civil-status

--- main/database/migrations/2024_07_17_125813_create_cert_residences_table.php
+++ main/database/migrations/2024_07_17_125813_create_cert_residences_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('age'); 
             $table->enum('gender', ['Male', 'Female', 'Other']);  
             $table->date('date'); 
+            $table->string('civil_status');
             $table->string('ORnumber')->nullable(); 
             $table->timestamps(); 
         });
